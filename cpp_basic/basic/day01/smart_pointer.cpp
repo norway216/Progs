39c2c92bca67d1d7922bb0c88e1fc59fe8ff5f25
norway216 <144678667+norway216@users.noreply.github.com>
2026-04-20 22:28:40 +0800

    [C++基础知识一] 使用图数据结构

--- cpp_basic/basic/day01/smart_pointer.cpp
+++ cpp_basic/basic/day01/smart_pointer.cpp
@@ -18,11 +18,39 @@ void f (const std::unique_ptr<Point>& p) {
     fmt::print("p1: {0}, {1}, {2}\n", ptr1->x, ptr1->y, ptr1->z);
     ptr1.reset(new Point{1,2,6});
     fmt::print("p1: {0}, {1}, {2}\n", ptr1->x, ptr1->y, ptr1->z);
+    // 释放资源
+    ptr1.release();
+}
+
+struct BufferHeader {
+    int head_size;
+    int current_size;
+    int capacity;
+};
+
+// 设计一个内存管理的函数
+void f1 (char* ptr, const size_t buffer_size) {
+    if (buffer_size <= 0) {
+        fmt::print("错误：函数参数不为负数...\n");
+        return;
+    }
+    ptr = new char[buffer_size];
+    memset(ptr, 0, buffer_size);
+
+    BufferHeader bh{(int)sizeof(BufferHeader), 0, (int)(buffer_size - (unsigned long)sizeof(BufferHeader))};
+    memcpy(ptr, &bh, sizeof(BufferHeader));
+
+    BufferHeader* bh_ptr = (BufferHeader*)ptr;
+    fmt::print("test: bufferheader size: {0}, current size: {1}, capacity: {2}\n",
+        bh_ptr->head_size, bh_ptr->current_size, bh_ptr->capacity);
 }
 
 int main () {
     std::unique_ptr<Point> p = std::make_unique<Point>(Point{1, 2, 3});
     f(p);
+
+    char* buffer = nullptr;
+    f1 (buffer, 1024);
 
     return 0;
 }
